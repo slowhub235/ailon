da9cf49a5f58454ff5bbeb358782b4154560791f
slowhub235 <arzhang.pazouki@gmail.com>
2026-05-05 20:17:51 -0700

    Update v1.lua

--- v1.lua
+++ v1.lua
@@ -1,2 +1,2 @@
-getgenv()SCRIPTKEY = ""
+getgenv().SCRIPTKEY = ""
 loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/04e3791970053b94b7d56db00eee2f80d9282f93a148a00fa8dbd18b357e67e7/download"))()
